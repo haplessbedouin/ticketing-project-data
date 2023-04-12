6e9dca7eb086124b65e0ffe7e4dce084401fa888
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-04-12 02:13:46 -0500

    delete function completed

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByUserName(String userName);
+    @Transactional
+    void deleteByUserName(String userName);
 
 }
