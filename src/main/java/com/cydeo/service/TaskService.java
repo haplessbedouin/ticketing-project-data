8b707738d1c0ec9ca7e39485c7b90647f6bb8a25
Ahmet Tugrul <ahmedsaid0173@gmail.com>
2023-04-15 01:56:48 -0500

    Task complete when the project completed

--- src/main/java/com/cydeo/service/TaskService.java
+++ src/main/java/com/cydeo/service/TaskService.java
@@ -2,6 +2,7 @@ package com.cydeo.service;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 
 import java.util.List;
 
@@ -15,6 +16,10 @@ public interface TaskService {
     int totalNonCompletedTask(String projectCode);
     int totalCompletedTask(String projectCode);
     void deleteByProject(ProjectDTO projectDTO);
+    void completeByProject(ProjectDTO projectDTO);
+    List<TaskDTO> listAllTasksByStatusIsNot(Status status);
+    List<TaskDTO> listAllTasksByStatus(Status status);
+
 
 
 
